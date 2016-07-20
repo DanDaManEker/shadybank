d6c451a1af9e5dba7b988ad495d7fe31ede565af
dandamaneker <eker.dan@gmail.com>
2016-07-20 11:15:22 +0300

    Changed the pi chart design added progressing animated bars for the money stolen sector.

--- app/script/app.js
+++ app/script/app.js
@@ -1,34 +1,10 @@
-/**
- * Created by Yuval on 6/20/2016.
- */
-var themeColor = ["blue","purple","red","green"];//this var holds the color of the theme//
-var panels = [
-    {title:" Money Transfer Status ", type: "chart"},
-    {title:" Rich VS Broke ", type: "chart"},
-    {title:" Online Slaves ", type: "table"},
-    {title:" Money Stolen ", type: "data"}
-    ];
-//console.log(panels[0].title);
-//for (index = 0; index < 4; index++) {
-//    console.log(panels[index].title);
-////}
-////for (x=0; x<=20;x++){
-////if (x>10){console.log( x+" larger than 10")}
-////    else if (x<10){console.log ( x+" smaller than 10")}
-////    else {console.log ("equals 10")}
-////}
-//for (x=0; x<=10;x++){
-//    console.log(x+"*9="+ x*9)}
-//}
-//this event has been triggered once the page is loaded.
+//page on load - bootstrapping the app!
 $(document).ready(function(){
     //attach click event to color change list
     $("ul li[value]").click(function(){
         colorChange($(this).attr("value"))
     });
-        $("#steal-money-btn").click(function(){
-            console.log("Button Was Clicked");
-        });
+
     //click on shady-switcher button
     $(".shady-switcher").click(function(){
         var switcherBtn = $(this);
@@ -36,172 +12,47 @@ $(document).ready(function(){
         if (switcherBtn.hasClass("glyphicon-menu-up"))
         {
             //if panel is expanded do something
-             panelBody.slideUp();
+            panelBody.slideUp();
             switcherBtn.addClass("glyphicon-menu-down").removeClass("glyphicon-menu-up");
         }
         else{
-            panelBody.slideDown()
+            panelBody.slideDown();
             switcherBtn.addClass("glyphicon-menu-up").removeClass("glyphicon-menu-down");
         }
     });
 
-                drawPieChart();//this is the pie chart
-                drawColumnChart();//this is the rich vs broke column chart
+    drawPieChart();//this is the pie chart
+    drawColumnChart();//this is the rich vs broke column chart
+    $(".progress-bar-ascending").animate({
+        width: "75%"
+    }, 2500);
+    //progress(100, $('#progress'));
 
-                getData();
-                shadyStolenFunds();
+    getData();
+    shadyStolenFunds();
+
+    //init the clock counter
+    var cdClock = $('#clock').countdown('2016/10/10 12:34:56')
+        .on('update.countdown', function(event) {
+        $(this).html(event.strftime('%D days %H:%M:%S'));
+    });
+    $("#steal-money-btn").on('click', function(){
+        cdClock.countdown('2016/12/31 12:34:56');
+        //need to zero down the funds/timer...we'll see
+        //need to add a next theft will be available on a date in between the one set and today
+    });
 }); //end of document ready
 
-function colorChange(panelColor){
+function colorChange(panelColor) {
 
     $(".shady-panel-heading").removeClass("background-color-blue background-color-red background-color-yellow background-color-green background-color-purple").addClass("background-color-" + panelColor);
 }
-function textColor(panelTitle) {
-    var choosePanelTitle = document.getElementsByClassName("panel-title");
-    for (i = 0; i < choosePanelTitle.length; i++) {
-        choosePanelTitle[i].style.color = panelTitle
-    }
-}
-function arrowDirection (dir){
-
-    console.log(dir);
-}
-//arrowDirection("left");
-//arrowDirection("right");
-
-
-//console.log(themeColor);
-//themeColor = "purple";
-//if (themeColor != "Red")
-//{console.log("The Theme Color Is "+themeColor);}
-//for (i= 0;i<themeColor.length;i++){console.log(themeColor [i])};//{means function}, [means array], {statment}
-
-
-//function numberChoice (x,y){
-//    console.log(x*y)
-//}
-//numberChoice(5,10);
-//
-//function dayOfWeek(dayOfWeek){
-//    console.log("today is "+dayOfWeek)
-//}
-//dayOfWeek("Mon");
-//dayOfWeek("Tue");
-//dayOfWeek("Shabes!");
-//
-//function request(person,plusAge){
-//    console.log(person+" is "+plusAge)
-//}
-//request("john",15);
-//request("Tom",35);
-//
-//function biggySmalls (x,y){
-//    if(x>y){
-//    console.log("Number one is bigger bro");}
-//    else if(x<y){
-//    console.log("Tupac is alive");}
-//    else{
-//    console.log("I'm broke");}
-//}
-//biggySmalls(10,9);
-//
-//function myMonth(myMonth){
-//    if (myMonth=="May")
-//    console.log("What Beautiful Weather");
-//    else if (myMonth)
-//    console.log(myMonth)
-//}
-//myMonth("May");
-//myMonth("September");
-//
-//$(".panel");
-//$("div");
-//$("#middle");
-//$("button.btn1");
-//
-//function hadasha(x){
-//    if(typeof(x)=="number")
-//    console.log(x+2);
-//    else
-//    console.log(typeof(x));
-//}
-//hadasha(3);
-//hadasha("tiruf");
-//hadasha(true);
-//
-//function cityName(x){
-//    console.log(x);
-//}
-//var city = "Barcelona";
-//cityName(city);
-//
-//function oneToSeven(x){
-//    if (x == 1)
-//    return "Sunday";
-//    if (x == 2)
-//        return "Monday";
-//    if (x == 3)
-//        return "Tuesday";
-//    if (x == 4)
-//        return "Wednesday";
-//    if (x == 5)
-//        return "Thursday";
-//    if (x == 6)
-//        return "Friday";
-//    if (x == 7)
-//        return "Shabat Hayom!!!";
-//}
-//var dayName = oneToSeven(7);
-//console.log(dayName);
-
-function status (onOff){
-    if (onOff=="on")
-    return "online-status";
-    if (onOff=="off")
-    return "offline-status";
-}
-//function myHeight(){
-//    $.ajax({
-//        method: "get",
-//        url: "data/height.json"
-//    })
-//        .done(function(series){
-//            //i don't get this part, its the only thing that's stucking me
-//        })
-//}
-var arr1 = [120, 130, 140, 150, 160];
-var arr2 = ["Charlie", "Shimon", "Zroobavel", "Mr.Miagi"];
-var peeps = [[1,2],[3,4],[5,6]];
-alert(peeps[0][0]);
-var specs = {
-    Developer: "Oneplus",
-    color: "Sand Black",
-    platform: "Android"
-};
-var drink ={
-    sugar: [true,false],
-    company: ["Shwepps","coca cola","martini"],
-    size: [250, 330, 500]
-}
-var personality = {
-    name: "John W Bush",
-    age: 64,
-    email: ["gbush@gmail.com", "gbushkingshlong@yahoo.com"],
-    address:{
-        houseName: "White House",
-        number: 35,
-        city: "Washington D.C"
-
-    }
-}
-
-
 
 function drawPieChart(){
     $.ajax({
-        method: "get",
-        url: "data/pieChart.json"
-    })
+            method: "get",
+            url: "data/pieChart.json"
+        })
         .done(function(series){
             var pieData = {
                 labels: ["Funds Currently Held", "Failed Transfers", "Complete Transfers"],
@@ -211,33 +62,40 @@ function drawPieChart(){
         })
 }
 
+function progress(percent, $element) {
+    var progressBarWidth = percent * $element.width() / 100;
+    $element.find('div').animate({ width: progressBarWidth }, 1000).html("$"+ percent);
+}
+//this is the animated progress bar
+
 function drawColumnChart() {
     $.ajax({
-       method: "get",
-       url: "data/barChart.json"
-   })
-       .done(function(series){
-           var barData = {
-               labels: ["Week 1", "Week 2", "Week 3", "Week 4"],
-               series:series
-           };
-           new Chartist.Bar("#barChart", barData,{
-               plugins:[
-                   Chartist.plugins.legend({
-                       legendNames:["Rich","Broke"]
-                   })
-               ]
-           });
-       })
+            method: "get",
+            url: "data/barChart.json"
+        })
+        .done(function(series){
+            var barData = {
+                labels: ["Week 1", "Week 2", "Week 3", "Week 4"],
+                series:series
+            };
+            new Chartist.Bar("#barChart", barData,{
+                plugins:[
+                    Chartist.plugins.legend({
+                        legendNames:["Rich","Broke"]
+                    })
+                ]
+            });
+        })
 
 
 }
-//nobody knows :( this function orders a request data
+
+//get the data for the slaves table
 function getData(){
     $.ajax({
-        method: "get",
-        url: "data/slaves.json"
-    })
+            method: "get",
+            url: "data/slaves.json"
+        })
         .done(function(res) {
             var tableHTML ='';
             var statusClass;
@@ -249,20 +107,20 @@ function getData(){
                 }
                 tableHTML +=
                     '<tr>'+
-                        '<td>'+ res[i].name +'</td>'+
-                        '<td><span class="'+statusClass+'" aria-hidden="true"></span></td>'+
-                        '<td>'+ res[i].location +'</td>'+
-                        '<td>'+ res[i].lastOnline +'</td>'+
-                        '</tr>'
+                    '<td>'+ res[i].name +'</td>'+
+                    '<td><span class="'+statusClass+'" aria-hidden="true"></span></td>'+
+                    '<td>'+ res[i].location +'</td>'+
+                    '<td>'+ res[i].lastOnline +'</td>'+
+                    '</tr>'
             }
             $("#shady-slaves").html(tableHTML);
         });
 }
 function shadyStolenFunds(){
     $.ajax({
-        method: "get",
-        url: "data/moneyStolen.json"
-    })
+            method: "get",
+            url: "data/moneyStolen.json"
+        })
         .done(function(moneyStolen){
             $("#shady-objective").html(moneyStolen.shadyFunds)
         })
